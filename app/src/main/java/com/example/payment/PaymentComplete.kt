2baa63109b334c5514d3893640329c603be30f05
IT21284816 <99232799+IT21284816@users.noreply.github.com>
2023-05-16 10:21:41 +0530

    initial commit

--- app/src/main/java/com/example/payment/PaymentComplete.kt
+++ app/src/main/java/com/example/payment/PaymentComplete.kt
@@ -12,7 +12,7 @@ class PaymentComplete : AppCompatActivity() {
         //title hide
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar()?.hide();
+        getSupportActionBar()?.hide()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_complete)
